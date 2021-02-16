5968b0216df0aecabdb1d3c1c67619cd4a3927dd
Ahmed <ahmed.jellouli@epfl.ch>
2021-02-16 23:29:32 +0800

    work on datajoint tutorial

---
+++ src/tutorial_tables.py
@@ -0,0 +1,13 @@
+import datajoint as dj
+
+schema = dj.schema('tutorial', locals())
+
+
+@schema
+class Mouse(dj.Manual):
+    definition = """
+      mouse_id: int                  # unique mouse id
+      ---
+      dob: date                      # mouse date of birth
+      sex: enum('M', 'F', 'U')    # sex of mouse - Male, Female, or Unknown/Unclassified
+      """
